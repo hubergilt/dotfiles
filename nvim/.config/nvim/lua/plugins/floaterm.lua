b3ef0c5198ce657c71e2b3187d1c28f31360db86
Huber Paul Gilt Lopez <hubergilt@hotmail.com>
2025-03-04 10:50:32 -0500

    update plantuml icons for thunar

--- nvim/.config/nvim/lua/plugins/floaterm.lua
+++ nvim/.config/nvim/lua/plugins/floaterm.lua
@@ -7,6 +7,7 @@ return {
 		vim.keymap.set("n", "<m-g>", ":FloatermNew --autoclose=0 gcc % -o %< && ./%<<CR>", {})
 		vim.keymap.set("n", "<m-j>", ":FloatermNew --autoclose=0 javac % && java $(basename % .java)<CR>", {})
 		vim.keymap.set("n", "<m-p>", ":FloatermNew --autoclose=0 python3 %<CR>", {})
+		vim.keymap.set("n", "<m-P>", ":FloatermNew --autoclose=0 poetry run view<CR>", {})
 		vim.keymap.set("n", "<m-T>", ":FloatermNew --autoclose=0 ant <CR>", {})
 	end,
 }
