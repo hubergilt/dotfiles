36494c2f69da8dc98338461d98bfffb5fe062a8f
Huber Paul Gilt Lopez <hubergilt@hotmail.com>
2025-09-03 17:39:43 -0500

    Update nvim theme config

--- nvim/.config/nvim/lua/plugins/catppuccin.lua
+++ nvim/.config/nvim/lua/plugins/catppuccin.lua
@@ -1,11 +1,23 @@
 return {
-  {
-    "catppuccin/nvim",
-    lazy = false,
-    name = "catppuccin",
-    priority = 1000,
-    config = function()
-      vim.cmd.colorscheme "catppuccin-mocha"
-    end
-  }
+    {
+        "catppuccin/nvim",
+        lazy = false,
+        name = "catppuccin",
+        priority = 1000,
+        config = function()
+            require("catppuccin").setup({
+                flavour = "latte", -- "latte", "frappe", "macchiato", or "mocha"
+                transparent_background = false,
+                integrations = {
+                    treesitter = true,
+                    native_lsp = { enabled = true },
+                    cmp = true,
+                    gitsigns = true,
+                    telescope = true,
+                    -- add others you use
+                },
+            })
+            vim.cmd.colorscheme("catppuccin")
+        end,
+    },
 }
