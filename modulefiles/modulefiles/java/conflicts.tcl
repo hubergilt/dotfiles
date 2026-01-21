55c606dd76196fe1807cb0d6d4c9a02c77197875
Huber Paul Gilt Lopez <hubergilt@hotmail.com>
2026-01-21 09:04:02 -0500

    Update java modulefiles config

--- modulefiles/modulefiles/java/conflicts.tcl
+++ modulefiles/modulefiles/java/conflicts.tcl
@@ -1,33 +1,9 @@
 set src [info script]
 proc proc_conflicts {} {
   global src
-  conflict /home/huber/modulefiles/java/temurin/jdk/8u422
-  conflict /home/huber/modulefiles/java/temurin/jdk/11
-  conflict /home/huber/modulefiles/java/temurin/jdk/17
-  conflict /home/huber/modulefiles/java/temurin/jdk/19
-  conflict /home/huber/modulefiles/java/temurin/jdk/21
-  conflict /home/huber/modulefiles/java/temurin/jre/8u422
-  conflict /home/huber/modulefiles/java/temurin/jre/11
-  conflict /home/huber/modulefiles/java/temurin/jre/17
-  conflict /home/huber/modulefiles/java/temurin/jre/19
-  conflict /home/huber/modulefiles/java/temurin/jre/21
-  conflict /home/huber/modulefiles/java/oracle/jdk/6u45
-  conflict /home/huber/modulefiles/java/oracle/jdk/8u202
-  conflict /home/huber/modulefiles/java/oracle/jdk/8u411
-  conflict /home/huber/modulefiles/java/oracle/jdk/11
-  conflict /home/huber/modulefiles/java/oracle/jdk/17
-  conflict /home/huber/modulefiles/java/oracle/jdk/19
-  conflict /home/huber/modulefiles/java/oracle/jdk/21
-  conflict /home/huber/modulefiles/java/oracle/jdk/23
-  conflict /home/huber/modulefiles/java/oracle/jre/8u202
-  conflict /home/huber/modulefiles/java/oracle/jre/8u411
-  conflict /home/huber/modulefiles/java/redhat/jdk/8u422
-  conflict /home/huber/modulefiles/java/redhat/jdk/11
-  conflict /home/huber/modulefiles/java/redhat/jdk/17
-  conflict /home/huber/modulefiles/java/redhat/jdk/21
-  conflict /home/huber/modulefiles/java/redhat/jre/8u422
-  conflict /home/huber/modulefiles/java/redhat/jre/11
-  conflict /home/huber/modulefiles/java/redhat/jre/17
-  conflict /home/huber/modulefiles/java/redhat/jre/21  
+  conflict /home/huber/modulefiles/java/oracle/jdk/8u481
+  conflict /home/huber/modulefiles/java/amazon/jdk/11.0.30
+  conflict /home/huber/modulefiles/java/redhat/jdk/17.0.17
+  conflict /home/huber/modulefiles/java/adoptium/jdk/25.0.1
 }
 
